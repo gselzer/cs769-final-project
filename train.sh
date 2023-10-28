b5ea9b4476b2142c55ef8730532159fff6bc800f
Gabriel Selzer <gjselzer@wisc.edu>
2023-10-28 17:11:19 -0500

    Use transformer-opt parameters

--- train.sh
+++ train.sh
@@ -6,11 +6,16 @@ CUDA_VISIBLE_DEVICES=0 fairseq-train \
     --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 \
     --lr 5e-4 --lr-scheduler inverse_sqrt --warmup-updates 4000 \
     --dropout 0.3 --weight-decay 0.0001 \
-    --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
+    --criterion label_smoothed_cross_entropy --label-smoothing 0.5 \
     --max-tokens 4096 \
     --eval-bleu \
     --eval-bleu-args '{"beam": 5, "max_len_a": 1.2, "max_len_b": 10}' \
     --eval-bleu-detok moses \
     --eval-bleu-remove-bpe \
     --eval-bleu-print-samples \
-    --best-checkpoint-metric bleu --maximize-best-checkpoint-metric
+    --best-checkpoint-metric bleu --maximize-best-checkpoint-metric \
+    --encoder-layers 5 --decoder-layers 5\
+    --encoder-attention-heads 2 --decoder-attention-heads 2\
+    --encoder-ffn-embed-dim 1024 --decoder-ffn-embed-dim 1024 \
+    --encoder-layerdrop 0.0 --decoder-layerdrop 0.2 \
+    --activation-dropout 0.3 \
