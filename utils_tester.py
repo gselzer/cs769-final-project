191035429cdd1d1f19e6fda4a658ceb9166a713d
Luke Neuendorf <79226650+lneuendorf@users.noreply.github.com>
2023-10-30 13:35:01 -0500

    Delete utils_tester.py
    No longer needed

--- utils_tester.py
+++
@@ -1,9 +0,0 @@
-import utils
-
-def main():
-    wa = utils.WordAligner()
-    wa.word_alignments(source_file='train.sample.en',
-                       target_file='train.sample.de',
-                       output_file='eflomal.en.de')
-
-if __name__ == "__main__": main()
