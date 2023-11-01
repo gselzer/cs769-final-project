577f390872145cb8a6f7755e75bc59d9bfad223f
Gabriel Selzer <gjselzer@wisc.edu>
2023-10-31 22:08:40 -0500

    Preprocess: Use train/test/valid data folder

--- preprocess.sh
+++ preprocess.sh
@@ -2,7 +2,7 @@
 # Binarize using fairseq
 rm -rf data-bin
 
-TEXT=.
+TEXT=data
 fairseq-preprocess --source-lang de --target-lang en \
     --trainpref $TEXT/train --validpref $TEXT/valid --testpref $TEXT/test \
     --destdir data-bin/iwslt14.tokenized.de-en \
