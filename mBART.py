03d292f6307122d734206d039a68d0bbe8b23c37
onezach <87786580+onezach@users.noreply.github.com>
2023-11-24 22:09:40 -0600

    clean up (not done though)

--- mBART.py
+++ mBART.py
@@ -2,14 +2,13 @@ from numpy.random import poisson
 import random
 from typing import List
 
-def mbart(src_files: List[str], tgt_files: List[str], p: float = 0.35, lambda_value: float = 3.5):
+def mbart(src_files: List[str], tgt_files: List[str], lambda_value: float = 3.5):
     """
     Apply noising to each sentence in the input file and save the result to the output file.
     
     Args:
         infile (str): Input file path.
         outfile (str): Output file path.
-        p (float): Probability of applying noising to each sentence.
         lambda_value (float): Lambda parameter for Poisson distribution.
     """
     for i in range(len(src_files)):
@@ -17,7 +16,7 @@ def mbart(src_files: List[str], tgt_files: List[str], p: float = 0.35, lambda_va
         with open(src_files[i], "r") as f:
             sentences = f.read().split("\n")
         
-        noised_sentences = [noising(sentence, p, lambda_value) for sentence in sentences]
+        noised_sentences = [noising(sentence, lambda_value) for sentence in sentences]
         random.shuffle(noised_sentences)
 
         print(noised_sentences)
@@ -26,13 +25,12 @@ def mbart(src_files: List[str], tgt_files: List[str], p: float = 0.35, lambda_va
             f.write("\n".join(noised_sentences))
 
 
-def noising(sentence: str, p, lambda_value):
+def noising(sentence: str, lambda_value):
     """
     Apply mBART-style noising to a sentence.
     
     Args:
         sentence (str): Input sentence.
-        p (float): Probability of applying each noising operation.
         lambda_value (float): Lambda parameter for Poisson distribution.
     
     Returns:
@@ -40,35 +38,15 @@ def noising(sentence: str, p, lambda_value):
     """
     words = sentence.split()
 
-    # Masking 35% of the words
-    # num_words_to_mask = int(len(words) * p)
-    # mask_start_indices = sorted(random.sample(range(len(words)), num_words_to_mask))
-
-    # for start_index in mask_start_indices:
-    #     span_length = max(1, int(poisson(lambda_value)))
-    #     end_index = min(len(words), start_index + span_length)
-    #     words[start_index:end_index] = ["[MASK]"] * (end_index - start_index)
-
-
     wl = len(words)
-
-    if wl == 0:
-        return ""
 
     mask_length = min(wl, poisson(lam=lambda_value)) #int(wl * p)
 
-    if mask_length == wl:
+    if mask_length == wl == 0:
         return ""
     
     mask_start = random.choice(range(wl - mask_length))
-    # mask_end = mask_start + mask_length
-    
-    # words[mask_start:mask_end] = ["[MASK]"] * mask_length
 
-    # for i in range(mask_length):
-    #     words[mask_start+i] = "<MASK>"
     del words[mask_start:mask_start+mask_length]
-    # # Randomly shuffle the order of words
-    # random.shuffle(words)
-    
+   
     return " ".join(words)
