3c0b40f48ab635cdc6639c7d2f71eacbe6d1fd5c
onezach <87786580+onezach@users.noreply.github.com>
2023-11-30 12:35:28 -0600

    revert num-workers

--- pretrain.sh
+++ pretrain.sh
@@ -36,7 +36,6 @@ CUDA_VISIBLE_DEVICES=0 fairseq-train \
     --criterion label_smoothed_cross_entropy --label-smoothing 0.5 \
     --max-tokens 4096 \
     --scoring sacrebleu \
-    --num-workers 4 \
     --eval-bleu \
     --eval-bleu-args '{"beam": 5, "max_len_a": 1.2, "max_len_b": 10}' \
     --eval-bleu-detok moses \
