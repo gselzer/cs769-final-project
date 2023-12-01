169580548a319931dab9451d36803344e6527920
onezach <87786580+onezach@users.noreply.github.com>
2023-12-01 15:49:06 -0600

    preliminary JD pipeline

--- experiments/optimized_de_en_joint-dropout.sh
+++ experiments/optimized_de_en_joint-dropout.sh
@@ -1,18 +1,16 @@
 NAME=$(basename "$0")
-SRC=de
-TGT=en
 
 # Step 1: Preprocess data
 python preprocess.py \
-    --src $SRC \
-    --tgt $TGT \
-    --mBART
+    --src de \
+    --tgt en \
+    --joint-dropout
 
-# Step 2: Pretrain
-bash ./train.sh $SRC $TGT data/pretrain
+# # Step 2: Pretrain
+# bash ./train.sh de en data/pretrain
 
 # Step 3: Finetune
-bash ./finetune.sh $SRC $TGT data
+bash ./finetune.sh de en data
 
 # Step 4: Evaluate
 if [ ! -d "experiments/results" ]; then
