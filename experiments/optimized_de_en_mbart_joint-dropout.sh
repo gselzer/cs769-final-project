3553a5f28868876776e2ed5d40d6befe61e7227b
Gabriel Selzer <gjselzer@wisc.edu>
2023-12-06 10:16:10 -0600

    Add mBART & joint dropout combo experiments

---
+++ experiments/optimized_de_en_mbart_joint-dropout.sh
@@ -0,0 +1,20 @@
+NAME=$(basename "$0")
+
+# Step 1: Preprocess data
+python preprocess.py \
+    --src de \
+    --tgt en \
+    --mBART \
+    --joint-dropout
+
+# Step 2: Pretrain
+bash ./train.sh de en data/pretrain
+
+# Step 3: Finetune
+bash ./finetune.sh de en data
+
+# Step 4: Evaluate
+if [ ! -d "experiments/results" ]; then
+    mkdir -p experiments/results
+fi
+bash ./evaluate.sh $NAME checkpoint_last
